2f1fa73768232b97e3677b68f067e7f1612cbbe7
Matthias Watermann <support@mwat.de>
2020-01-23 22:15:12 +0100

    * refactored filename handling;

--- uploadhandler.go
+++ uploadhandler.go
@@ -1,5 +1,5 @@
 /*
-   Copyright © 2019 M.Watermann, 10247 Berlin, Germany
+   Copyright © 2019, 2020 M.Watermann, 10247 Berlin, Germany
                    All rights reserved
                EMail : <support@mwat.de>
 */
@@ -39,6 +39,59 @@ type (
 		ms int64                    // max. upload file size
 	}
 )
+
+// `newFilename()` checks the filename extension of `aFilename` and
+// returns a filename for the uploaded image.
+//
+//	`aFilename` The complete filename received with the upload request.
+//	`aExtension` The filename extension based on the determined file type.
+func (uh *TUploadHandler) newFilename(aFilename, aExtension string) string {
+	switch aExtension {
+	case ".asc":
+		aExtension = ".txt"
+	case ".jpg":
+		aExtension = ".jpeg"
+	case ".mpg":
+		aExtension = ".mpeg"
+		//XXX possibly re-map more extensions
+	default:
+		break
+	}
+
+	// Preserve an (arbitrary) number of file extensions:
+	switch strings.ToLower(filepath.Ext(aFilename)) {
+	case aExtension:
+		aExtension = ""
+	case ".amr", ".arj", ".avi", ".azw3",
+		".bak", ".bibtex", ".bz2",
+		".cfg", ".com", ".conf", ".css", ".csv",
+		".db", ".deb", ".doc", ".docx", ".dia",
+		".epub", ".exe", ".flv",
+		".gif", ".gz", ".htm", ".html",
+		".ics", ".iso", ".jar", ".jpeg", ".json",
+		".log", ".md", ".mobi", ".mp3", ".mp4", ".mpeg",
+		".odf", ".odg", ".odp", ".ods", ".odt", ".otf", ".oxt",
+		".pas", ".pdf", ".php", ".pl", ".ppd", ".ppt", ".pptx",
+		".rip", ".rpm",
+		".sh", ".shtml", ".spk", ".sql", ".sxg", ".sxw",
+		".ttf", ".txt", ".vbox", ".vmdk", ".vcs",
+		".wav", ".xhtml", ".xls", ".xpi", ".xsl", ".zip":
+		aExtension = ""
+	}
+
+	if 0 < len(aExtension) {
+		// If we change the given filename extension remove it:
+		if i := strings.LastIndexByte(aFilename, '.'); 0 < i {
+			aFilename = aFilename[:i]
+		}
+	}
+
+	return filepath.Join(uh.dd,
+		fmt.Sprintf(`%x_%s%s`,
+			time.Now().UnixNano(),
+			strings.ReplaceAll(aFilename, ` `, `_`),
+			aExtension))
+} // newFilename()
 
 // `returnError()` sends a (possibly customised) error message
 // to the remote user.
@@ -80,7 +133,7 @@ func (uh *TUploadHandler) ServeUpload(aWriter http.ResponseWriter,
 	}
 
 	// parse and validate file and post parameters
-	inFile, fheader, err := aRequest.FormFile(uh.fn)
+	inFile, fHeader, err := aRequest.FormFile(uh.fn)
 	if nil != err {
 		return "Error retrieving file", http.StatusUnprocessableEntity
 	}
@@ -105,43 +158,8 @@ func (uh *TUploadHandler) ServeUpload(aWriter http.ResponseWriter,
 		return "Can't read file type", http.StatusUnsupportedMediaType
 	}
 
-	switch fileExt {
-	case ".asc":
-		fileExt = ".txt"
-	case ".jpg":
-		fileExt = ".jpeg"
-	case ".mpg":
-		fileExt = ".mpeg"
-		//TODO possibly re-map more extensions
-	default:
-		break
-	}
-
-	// preserve an (arbitrary) number of file extensions:
-	switch strings.ToLower(filepath.Ext(fheader.Filename)) {
-	case fileExt:
-		fileExt = ""
-	case ".amr", ".arj", ".avi", ".azw3",
-		".bak", ".bibtex", ".bz2",
-		".cfg", ".com", ".conf", ".css", ".csv",
-		".db", ".deb", ".doc", ".docx", ".dia",
-		".epub", ".exe", ".flv",
-		".gif", ".gz", ".htm", ".html",
-		".ics", ".iso", ".jar", ".jpeg", ".json",
-		".log", ".md", ".mobi", ".mp3", ".mp4", ".mpeg",
-		".odf", ".odg", ".odp", ".ods", ".odt", ".otf", ".oxt",
-		".pas", ".pdf", ".php", ".pl", ".ppd", ".ppt", ".pptx",
-		".rip", ".rpm",
-		".sh", ".shtml", ".spk", ".sql", ".sxg", ".sxw",
-		".ttf", ".txt", ".vbox", ".vmdk", ".vcs",
-		".wav", ".xhtml", ".xls", ".xpi", ".xsl", ".zip":
-		fileExt = ""
-	}
-
-	newPathFn := filepath.Join(uh.dd, fmt.Sprintf("%x_%s%s",
-		time.Now().UnixNano(),
-		strings.ReplaceAll(fheader.Filename, " ", "_"),
-		fileExt))
+	// construct a filename for the uploaded file
+	newPathFn := uh.newFilename(fHeader.Filename, fileExt)
 
 	// copy file into the configured destination directory
 	newFile, err := os.OpenFile(newPathFn, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0640) // #nosec G302
@@ -231,7 +249,7 @@ func urlPath(aURL string) string {
 //
 //	`aUpURL` The URL uploads are POSTed to.
 //
-//	`aNextURL` The URL to redirect the user after a asuccessful upload.
+//	`aNextURL` The URL to redirect the user after a successful upload.
 //
 //	`aMaxSize` The max. accepted size of uploaded files.
 //
--- uploadhandler_test.go
+++ uploadhandler_test.go
@@ -1,12 +1,18 @@
 /*
-   Copyright © 2019 M.Watermann, 10247 Berlin, Germany
+   Copyright © 2019, 2020 M.Watermann, 10247 Berlin, Germany
                    All rights reserved
                EMail : <support@mwat.de>
 */
 
 package uploadhandler
 
-import "testing"
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+import (
+	"path/filepath"
+	"regexp"
+	"testing"
+)
 
 func Test_urlPath(t *testing.T) {
 	type args struct {
@@ -34,3 +40,32 @@ func Test_urlPath(t *testing.T) {
 		})
 	}
 } // Test_urlPath()
+
+func TestTUploadHandler_newFilename(t *testing.T) {
+	dir, _ := filepath.Abs(`./`)
+	h1 := NewHandler(dir, `Upload`, 1024*1024)
+	re := regexp.MustCompile(dir + `/[a-f0-9]+_.+\.[a-z]+$`)
+	type args struct {
+		aFilename  string
+		aExtension string
+	}
+	tests := []struct {
+		name   string
+		fields *TUploadHandler
+		args   args
+	}{
+		// TODO: Add test cases.
+		// {" 0", h1, args{``, ``}},
+		{" 1", h1, args{`profile.asc`, `.asc`}},
+		{" 2", h1, args{`profi.jpg`, `.jpg`}},
+		{" 3", h1, args{`x.mpg`, `.mpg`}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			uh := tt.fields
+			if got := uh.newFilename(tt.args.aFilename, tt.args.aExtension); !re.Match([]byte(got)) {
+				t.Errorf("TUploadHandler.newFilename() = '%v'", got)
+			}
+		})
+	}
+} // TestTUploadHandler_newFilename()
